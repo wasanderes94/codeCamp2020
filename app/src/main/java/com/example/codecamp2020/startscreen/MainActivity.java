f32629e759ac74f54d77a10a5302fdcf410f3393
ASMItteahd <ittehad.akash@gmail.com>
2020-08-06 19:15:40 +0200

    Added Slider

--- app/src/main/java/com/example/codecamp2020/startscreen/MainActivity.java
+++ app/src/main/java/com/example/codecamp2020/startscreen/MainActivity.java
@@ -7,14 +7,20 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.ImageView;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.codecamp2020.map.MapsActivity;
 import com.example.codecamp2020.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private TextView textView3;
     private ImageView imageView2;
-    private TextView location;
+    private ImageSlider imageSlider;
     private TextView infectionTextView;
 
     @Override
@@ -24,9 +30,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         textView3 = (TextView) findViewById(R.id.Localisation);
         imageView2 = (ImageView) findViewById(R.id.imageView2);
-        location = (TextView) findViewById(R.id.advices);
+        imageSlider = (ImageSlider) findViewById(R.id.image_slider);
         infectionTextView = (TextView) findViewById(R.id.infectionTextView);
         findViewById(R.id.emergencyButton).setOnClickListener(this);
+
+
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.mask,"Always Wear Mask Outside Home", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.distance,"Keep At least 2 meter Distance From Others",ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.washing_hands,"After Every 30 Minutes Wash Those Hands", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.mobile_phone,"Clean Your belongings Frequently", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.family,"Spend More Time With Your Family", ScaleTypes.CENTER_CROP));
+        imageSlider.setImageList(slideModels, ScaleTypes.CENTER_CROP);
+
+
+
     }
 
     @Override
